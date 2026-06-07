787ff40abe9e580c87b8a19049bc6a7b1287e4f7
Murtaza Ali <murtaza.ali@example.com>
2026-06-07 19:28:30 +0500

    Add version hash for zeroscope-v2-xl

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -54,7 +54,7 @@ export async function POST(req: Request) {
     let videoUrl = "";
     try {
         const output = await replicate.run(
-            "anotherjesse/zeroscope-v2-xl",
+            "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
             {
                 input: {
                     prompt: prompt,
